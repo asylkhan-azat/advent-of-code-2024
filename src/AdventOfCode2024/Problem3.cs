60958aaf7f629b06407d862f8780128f16a27aff
Asylkhan Azat <asylkhan.azat@gmail.com>
2024-12-04 03:01:31 +0500

    Solve Problem3 part 2

--- src/AdventOfCode2024/Problem3.cs
+++ src/AdventOfCode2024/Problem3.cs
@@ -20,36 +20,111 @@ public sealed partial class Problem3 : IProblem
             case 1:
                 SolvePartOne();
                 break;
+            
+            case 2:
+                SolvePartTwo();
+                break;
         }
     }
 
-    private void SolvePartOne()
+    private void SolvePartTwo()
     {
-        var total = 0;
+        var machine = new Machine(ignoreSwitches: false);
 
-        foreach (var operation in MulOperation.FromFile(_path))
+        foreach (var instruction in ReadInstructionsFromFile(_path))
         {
-            total += operation.LhsOperand * operation.RhsOperand;
+            instruction.InvokeOn(machine);
         }
-        
-        _writer.WriteLine(total);
+
+        _writer.WriteLine(machine.Total);
     }
     
-    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
-    private static partial Regex MulPattern { get; }
-
-    private readonly record struct MulOperation(int LhsOperand, int RhsOperand)
+    private void SolvePartOne()
     {
-        public static IEnumerable<MulOperation> FromFile(string path)
-        {
-            var input = File.ReadAllText(path);
+        var machine = new Machine(ignoreSwitches: true);
 
-            foreach (Match match in MulPattern.Matches(input))
+        foreach (var instruction in ReadInstructionsFromFile(_path))
+        {
+            instruction.InvokeOn(machine);
+        }
+
+        _writer.WriteLine(machine.Total);
+    }
+
+    [GeneratedRegex(@"((mul)\((\d+),(\d+)\))|(((do)|(don't))\(\))")]
+    private static partial Regex InstructionPattern { get; }
+
+    private static IEnumerable<IInstruction> ReadInstructionsFromFile(string path)
+    {
+        var input = File.ReadAllText(path);
+
+        foreach (Match match in InstructionPattern.Matches(input))
+        {
+            if (match.Groups[2].ValueSpan is "mul")
             {
-                yield return new MulOperation(
-                    int.Parse(match.Groups[1].ValueSpan),
-                    int.Parse(match.Groups[2].ValueSpan));
+                yield return new Mul(
+                    int.Parse(match.Groups[3].ValueSpan),
+                    int.Parse(match.Groups[4].ValueSpan));
             }
+            else switch (match.Groups[6].ValueSpan)
+            {
+                case "do":
+                    yield return Switch.Enable;
+                    break;
+                case "don't":
+                    yield return Switch.Disable;
+                    break;
+            }
+        }
+    }
+
+    private sealed class Machine
+    {
+        private readonly bool _ignoreSwitches;
+        
+        public Machine(bool ignoreSwitches)
+        {
+            _ignoreSwitches = ignoreSwitches;
+        }
+        
+        public int Total { get; private set; }
+
+        private bool _disabled;
+
+        public void VisitMul(Mul mul)
+        {
+            if (_disabled) return;
+            Total += mul.LhsOperand * mul.RhsOperand;
+        }
+
+        public void VisitSwitch(Switch @switch)
+        {
+            if (_ignoreSwitches) return;
+            _disabled = @switch.Disabled;
+        }
+    }
+
+    private interface IInstruction
+    {
+        void InvokeOn(Machine machine);
+    }
+
+    private sealed record Switch(bool Disabled) : IInstruction
+    {
+        public static readonly Switch Enable = new(Disabled: false);
+        public static readonly Switch Disable = new(Disabled: true);
+        
+        public void InvokeOn(Machine machine)
+        {
+            machine.VisitSwitch(this);
+        }
+    }
+
+    private sealed record Mul(int LhsOperand, int RhsOperand) : IInstruction
+    {
+        public void InvokeOn(Machine machine)
+        {
+            machine.VisitMul(this);
         }
     }
 }
